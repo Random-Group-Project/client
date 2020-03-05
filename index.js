8cc965b4e02997f80ba340c2e61e5c41a0376540
Fadhilah Metra <fadhilahmetra@gmail.com>
2020-03-06 02:17:48 +0700

    Improved Google's OAuth handling

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  console.log("ID: " + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log("Name: " + profile.getName());
-  console.log("Image URL: " + profile.getImageUrl());
-  console.log("Email: " + profile.getEmail()); // This is null if the 'email' scope is not present.
+  // let profile = googleUser.getBasicProfile();
+  // console.log("ID: " + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  // console.log("Name: " + profile.getName());
+  // console.log("Image URL: " + profile.getImageUrl());
+  // console.log("Email: " + profile.getEmail()); // This is null if the 'email' scope is not present.
+  let id_token = googleUser.getAuthResponse().id_token;
+  console.log("User successfully logged in.");
+  localStorage.setItem("token", id_token);
+  // sent to server
+  $.ajax({
+    method: "GET",
+    url: "http://localhost:3000/users/googlesign",
+    headers: {
+      token: localStorage.getItem("token")
+    }
+  })
+  .done(response => console.log(response))
+  .fail(err => console.log(err))
+  .always(console.log(`Currently sending data...`))
 }
 
 function signOut() {
